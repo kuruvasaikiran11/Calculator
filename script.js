f7183311f466c1e48dcfbec8263e327118cb6ef0
kuruvasaikiran11 <kuruvasaikiran11@gmail.com>
2023-09-20 21:26:24 +0530

    Calculator Functionality Added

--- script.js
+++ script.js
@@ -25,4 +25,89 @@ document.addEventListener("DOMContentLoaded", function(){
             }
         });
     });
+
+    function calculateResult() {
+        console.log("CLicked");
+        const expression = inputOperationElement.value;
+        try {
+            const calcValue = evaluateExpression(expression);
+            // console.log(calcValue)
+            result.innerHTML = calcValue;
+        } catch (error) {
+            result.innerHTML = "Error";
+        }
+    }
+
+    function evaluateExpression(expression) {
+        // Split the expression into numbers and operators
+        const tokens = expression.replace(/\s+/, "").match(/(\d+\.\d+|\d+|[+\-*/])/g);
+
+        if (!tokens) {
+            throw new Error('Invalid expression');
+        }
+
+        // Define operator precedence
+        const precedence = {
+            '+': 1,
+            '-': 1,
+            '*': 2,
+            '/': 2
+        };
+
+        const output = [];
+        const operatorStack = [];
+
+        for (const token of tokens) {
+            if (!isNaN(parseFloat(token))) {
+                output.push(parseFloat(token));
+            } else if (['+', '-', '*', '/'].includes(token)) {
+                while (
+                    operatorStack.length > 0 &&
+                    precedence[operatorStack[operatorStack.length - 1]] >= precedence[token]
+                ) {
+                    output.push(operatorStack.pop());
+                }
+                operatorStack.push(token);
+            }
+        }
+
+        while (operatorStack.length > 0) {
+            output.push(operatorStack.pop());
+        }
+
+        const resultStack = [];
+
+        for (const token of output) {
+            if (typeof token === 'number') {
+                resultStack.push(token);
+            } else {
+                const num2 = resultStack.pop();
+                const num1 = resultStack.pop();
+                switch (token) {
+                    case '+':
+                        resultStack.push(num1 + num2);
+                        break;
+                    case '-':
+                        resultStack.push(num1 - num2);
+                        break;
+                    case '*':
+                        resultStack.push(num1 * num2);
+                        break;
+                    case '/':
+                        if (num2 === 0) {
+                            throw new Error('Division by zero');
+                        }
+                        resultStack.push(num1 / num2);
+                        break;
+                }
+            }
+        }
+
+        if (resultStack.length !== 1 || isNaN(resultStack[0])) {
+            throw new Error('Invalid expression');
+        }
+
+        return resultStack[0];
+    }
+        
 });
